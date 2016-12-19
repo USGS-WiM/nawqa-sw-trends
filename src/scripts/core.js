7aed14152cac20db260f257a87f8871dc82bac87
Nick Estes <njestes@usgs.gov>
2016-12-19 14:51:31 -0800

    added handsontable library and dummy table in trends results. For some reason on available after clicking on the modal

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -970,6 +970,23 @@ require([
 
         function showTableModal () {
             $('#tableModal').modal('show');
+
+
+
+            var data = [
+                ["", "sample1", "sample2", "sample3", "sample4"],
+                ["2016", 10, 11, 12, 13],
+                ["2017", 20, 11, 14, 13],
+                ["2018", 30, 15, 12, 13]
+            ];
+
+            var container = document.getElementById('tableDiv');
+            $("#tableDiv").html("");
+            var hot = new Handsontable(container, {
+                data: data,
+                rowHeaders: true,
+                colHeaders: true
+            });
         }
         $('#table').click(function(){
             showTableModal();
